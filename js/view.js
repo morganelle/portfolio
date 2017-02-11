7e628d84d15739a51cc470a03aafcc6e2e2c5d06
Morgan Nomura <morganelle@gmail.com>
2017-02-11 13:17:25 -0800

    added subtle color and iteractivity to nav items, added footer, fixed template visibility issue

--- js/view.js
+++ js/view.js
@@ -1,7 +1,9 @@
 'use strict';
 
-$('ul#page-nav').on('click', 'li', function() {
-  $('#page-content section').hide();
-  $('#' + $(this).data('section')).show();
-  $('#' + $(this).data('section')).children().show(); // works if there are nested sections?
+$('.top-nav:first-child').on('click', 'li', function() {
+  $('#page-nav li').removeClass('active-nav-item'); // removes active designation from top nav li
+  $(this).addClass('active-nav-item'); // adds active designation to clicked nav li
+  $('#page-content section').hide(); // hides all sections on click
+  $('#' + $(this).data('section')).show(); // shows the section that matches the data-section value
+  $('#' + $(this).data('section')).children().not('.template').show(); // works if there are nested sections?
 });
