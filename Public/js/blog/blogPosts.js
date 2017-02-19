6b23ee3b2d6b209d76775092944de8e6f15d17ab
Morgan Nomura <morganelle@gmail.com>
2017-02-19 10:19:45 -0800

    added closures to blogPosts.js and blogViews.js

--- Public/js/blog/blogPosts.js
+++ Public/js/blog/blogPosts.js
@@ -1,65 +1,65 @@
 'use strict';
 
-console.log('blog posts has loaded');
+(function(module) {
+  // Array of blog post objects
+  var blogPosts = [];
 
-// Array of blog post objects
-var blogPosts = [];
-
-// Constructor
-function BlogContent(opts) {
-  this.title = opts.title;
-  this.publishedDate = (new Date(opts.publishedDate)).toDateString();
-  this.author = opts.author;
-  this.postContent = opts.postContent;
-  this.category = opts.category;
-}
-
-// Gets the Handlebar template and makes a function
-BlogContent.prototype.populateTemplate = function() {
-  var source = $('#blog-template').html();
-  var template = Handlebars.compile(source);
-  return template(this);
-}
-
-BlogContent.load = function(newData) {
-  console.log('BlogContent load method is running');
-  newData.sort(function(a,b) {
-    return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
-  });
-  newData.forEach(function(post) {
-    blogPosts.push(new BlogContent(post));
-  })
-}
-
-BlogContent.toHtml = function(blogs) {
-  blogs.forEach(function(item) {
-    $('#blog').append(item.populateTemplate());
-  })
-}
-
-// gets etag from HEAD and compares it with etag set in localStorage
-$.ajax({
-  type: 'HEAD',
-  async: true,
-  url: '../data/blogContent.json',
-  success: function(data, message, xhr) {
-    let etag = xhr.getResponseHeader('ETag');
-    if (etag !== localStorage.storageETag) { // if the etags don't match, populate blog content from JSON
-      console.log('etags do not match, loading from JSON file');
-      $.getJSON('../data/blogContent.json', function(data) {
-        BlogContent.load(data);
-        BlogContent.toHtml(blogPosts);
-        localStorage.setItem('blogcontent', JSON.stringify(data));
-      });
-    }
-    else { // otherwise, get the blog content from localStorage
-      console.log('etags match, loading from local storage');
-      BlogContent.load(JSON.parse(localStorage.blogcontent));
-      BlogContent.toHtml(blogPosts);
-    }
-    localStorage.setItem('storageETag', etag);
-  },
-  fail: function() {
-    console.log('fail'); // wondering: should the fail condition just grab content from the JSON file?
+  // Constructor
+  function BlogContent(opts) {
+    this.title = opts.title;
+    this.publishedDate = (new Date(opts.publishedDate)).toDateString();
+    this.author = opts.author;
+    this.postContent = opts.postContent;
+    this.category = opts.category;
   }
-});
+
+  // Gets the Handlebar template and makes a function
+  BlogContent.prototype.populateTemplate = function() {
+    var source = $('#blog-template').html();
+    var template = Handlebars.compile(source);
+    return template(this);
+  }
+
+  BlogContent.load = function(newData) {
+    newData.sort(function(a,b) {
+      return (new Date(b.publishedDate)) - (new Date(a.publishedDate));
+    });
+    newData.forEach(function(post) {
+      blogPosts.push(new BlogContent(post));
+    })
+  }
+
+  BlogContent.toHtml = function(blogs) {
+    blogs.forEach(function(item) {
+      $('#blog').append(item.populateTemplate());
+    })
+  }
+
+  // gets etag from HEAD and compares it with etag set in localStorage
+  $.ajax({
+    type: 'HEAD',
+    async: true,
+    url: '../data/blogContent.json',
+    success: function(data, message, xhr) {
+      let etag = xhr.getResponseHeader('ETag');
+      if (etag !== localStorage.storageETag) { // if the etags don't match, populate blog content from JSON
+        console.log('etags do not match, loading from JSON file');
+        $.getJSON('../data/blogContent.json', function(data) {
+          BlogContent.load(data);
+          BlogContent.toHtml(blogPosts);
+          localStorage.setItem('blogcontent', JSON.stringify(data));
+        });
+      }
+      else { // otherwise, get the blog content from localStorage
+        console.log('etags match, loading from local storage');
+        BlogContent.load(JSON.parse(localStorage.blogcontent));
+        BlogContent.toHtml(blogPosts);
+      }
+      localStorage.setItem('storageETag', etag);
+    },
+    fail: function() {
+      console.log('fail'); // wondering: should the fail condition just grab content from the JSON file?
+    }
+  });
+  module.BlogContent = BlogContent;
+})(window);
--- Public/js/blog/blogViews.js
+++ Public/js/blog/blogViews.js
@@ -1,39 +1,41 @@
 'use strict';
 
-// Creates category inputs on blog-fields form
-function populateCategories() {
-  var source = $('#category-input-template').html();
-  var template = Handlebars.compile(source);
-  var inputs = template(context);
-  console.log(inputs);
-  $('label#category-checkboxes').append(inputs);
-}
+(function() {
+  // Creates category inputs on blog-fields form
+  function populateCategories() {
+    var source = $('#category-input-template').html();
+    var template = Handlebars.compile(source);
+    var inputs = template(context);
+    console.log(inputs);
+    $('label#category-checkboxes').append(inputs);
+  }
 
-populateCategories();
+  populateCategories();
 
-// Hides for preview section by default
-$('#preview').hide();
+  // Hides for preview section by default
+  $('#preview').hide();
 
-// Registers event on blog-fields form
-$('#blog-fields').on('change', function() {
-  console.log('blog fields change');
-  $('#preview').show();
-  $('article.blog-post').remove();
-  blogPreview();
-});
-
-// Get information from form
-function blogPreview() {
-  // instantiate new blog object
-  var blog = new BlogContent({
-    title: $('#post-title').val(),
-    publishedDate: (new Date($('#post-date').val())).toDateString(),
-    author: $('#post-author').val(),
-    postContent: $('#post-content').val(),
-    category: ['category']
+  // Registers event on blog-fields form
+  $('#blog-fields').on('change', function() {
+    console.log('blog fields change');
+    $('#preview').show();
+    $('article.blog-post').remove();
+    blogPreview();
   });
-  console.log(blog);
-  // Append blog content to preview section
-  $('#preview').append(blog.populateTemplate());
-  $('#blog-json').val(JSON.stringify(blog) + ',');
-}
+
+  // Get information from form
+  function blogPreview() {
+    // instantiate new blog object
+    var blog = new BlogContent({
+      title: $('#post-title').val(),
+      publishedDate: (new Date($('#post-date').val())).toDateString(),
+      author: $('#post-author').val(),
+      postContent: $('#post-content').val(),
+      category: ['category']
+    });
+    console.log(blog);
+    // Append blog content to preview section
+    $('#preview').append(blog.populateTemplate());
+    $('#blog-json').val(JSON.stringify(blog) + ',');
+  }
+})();
