8ade7fccd42045519507e00fa2443a0e0806a232
Morgan Nomura <morganelle@gmail.com>
2017-02-23 21:00:43 -0800

    adding proxy

--- Public/scripts/models/repos.js
+++ Public/scripts/models/repos.js
@@ -4,20 +4,12 @@
   const repos = {};
 
   repos.all = [];
-  repos.contribut = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      type: 'GET',
-      url: 'https://api.github.com/user/repos',
-      headers: {
-        Authorization: `token ${githubtoken}`
-      }
-    }).then(data => repos.all = data)
-    .then(function() {
-      callback();
-    })
-  }
+    $.get('github/user/repos')
+    .then(data => repos.all = data, err => console.error(err))
+    .then(callback);
+  };
 
   // function for filtering repos.all
   repos.with = attr => repos.all.filter(repo => repo[attr]);
