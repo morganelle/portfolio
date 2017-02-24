3e8114ccef7fd5c5e6f4bd3a880aaef7301c3834
Morgan Nomura <morganelle@gmail.com>
2017-02-23 21:16:39 -0800

    trying to remove github call

--- Public/scripts/models/repos.js
+++ Public/scripts/models/repos.js
@@ -5,11 +5,11 @@
 
   repos.all = [];
 
-  repos.requestRepos = function(callback) {
-    $.get('github/user/repos')
-    .then(data => repos.all = data, err => console.error(err))
-    .then(callback);
-  };
+  // repos.requestRepos = function(callback) {
+  //   $.get('github/user/repos')
+  //   .then(data => repos.all = data, err => console.error(err))
+  //   .then(callback);
+  // };
 
   // function for filtering repos.all
   repos.with = attr => repos.all.filter(repo => repo[attr]);
